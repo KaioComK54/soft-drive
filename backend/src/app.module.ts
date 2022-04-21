5912291241032dc4b8ceb178e562cac27516825c
KaioMorais34 <kaio@zeester.com.br>
2022-04-21 16:56:15 -0300

    Adds request rate limit to all endpoints

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { ApiLoggerMiddleware } from './modules/shared/middlewares/logger.middleware';
 import { LoggerModule } from './modules/shared/logger/logger.module';
 import { UserModule } from './modules/user/user.module';
@@ -9,6 +11,10 @@ import { AppController } from './app.controller';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: parseInt(process.env.LIMIT_OF_TIME),
+      limit: parseInt(process.env.LIMIT_OF_REQUESTS),
+    }),
     ConfigModule.forRoot(),
     UserModule,
     AuthModule,
@@ -16,7 +22,12 @@ import { AppController } from './app.controller';
     LoggerModule,
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
